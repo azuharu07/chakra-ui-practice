aab4c2b76d2977c698ec13b92c836f42f6ee2449
azuharu07 <azuha07i@gmail.com>
2022-08-01 01:56:58 +0900

    :pencil2: Copy Zenn scrap UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      default: "#3ea8ff",
+      darker: "#0f83fd",
+      bgc: "#edf2f7",
+    },
+    gray: {
+      default: "#93a5b1",
+      darker: "#6e7b85",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
